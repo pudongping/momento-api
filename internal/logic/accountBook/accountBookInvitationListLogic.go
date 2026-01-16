88cd7230db75f0c7ad61c531cb24f93011fc009d
Alex Pu <276558492@qq.com>
2026-01-16 13:49:35 +0800

    Update accountBookInvitationListLogic.go

--- internal/logic/accountBook/accountBookInvitationListLogic.go
+++ internal/logic/accountBook/accountBookInvitationListLogic.go
@@ -4,11 +4,13 @@ import (
 	"context"
 
 	"github.com/Masterminds/squirrel"
+	"github.com/pkg/errors"
 	"github.com/pudongping/momento-api/coreKit/ctxData"
 	"github.com/pudongping/momento-api/coreKit/errcode"
 	"github.com/pudongping/momento-api/internal/svc"
 	"github.com/pudongping/momento-api/internal/types"
 	"github.com/spf13/cast"
+	"github.com/zeromicro/go-zero/core/mr"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -38,56 +40,51 @@ func (l *AccountBookInvitationListLogic) AccountBookInvitationList(req *types.Ac
 
 	invitations, err := l.svcCtx.AccountBookInvitationsModel.FindAll(l.ctx, rowBuilder)
 	if err != nil {
-		l.Logger.Errorf("AccountBookInvitationList FindAll error: %v", err)
-		return nil, errcode.DBError.Msgr("获取邀请列表失败")
+		return nil, errcode.DBError.Msgr("获取邀请列表失败").WithError(errors.Wrap(err, "获取邀请列表失败"))
 	}
 
-	resp = make([]types.AccountBookInvitationListResp, 0)
 	if len(invitations) == 0 {
-		return resp, nil
+		return nil, nil
 	}
 
-	// 2. 收集账本ID和邀请人ID
+	// 2. 收集去重后的账本ID和邀请人ID
 	bookIDs := make([]uint64, 0, len(invitations))
 	inviterUIDs := make([]uint64, 0, len(invitations))
+	bookIDMap := make(map[uint64]struct{})
+	inviterUIDMap := make(map[uint64]struct{})
+
 	for _, inv := range invitations {
-		bookIDs = append(bookIDs, inv.BookId)
-		inviterUIDs = append(inviterUIDs, inv.InviterUid)
-	}
-
-	// 3. 批量查询账本信息
-	// 注意：go-zero生成的model通常没有批量查询，这里使用SelectBuilder构建 IN 查询
-	var booksMap = make(map[uint64]string)
-	if len(bookIDs) > 0 {
-		bookBuilder := l.svcCtx.AccountBooksModel.SelectBuilder().Where(squirrel.Eq{"book_id": bookIDs})
-		books, err := l.svcCtx.AccountBooksModel.FindAll(l.ctx, bookBuilder)
-		if err != nil {
-			l.Logger.Errorf("AccountBookInvitationList FindAll Books error: %v", err)
-			// 不阻断，继续
-		} else {
-			for _, book := range books {
-				booksMap[book.BookId] = book.Name
-			}
+		if _, ok := bookIDMap[inv.BookId]; !ok {
+			bookIDs = append(bookIDs, inv.BookId)
+			bookIDMap[inv.BookId] = struct{}{}
+		}
+		if _, ok := inviterUIDMap[inv.InviterUid]; !ok {
+			inviterUIDs = append(inviterUIDs, inv.InviterUid)
+			inviterUIDMap[inv.InviterUid] = struct{}{}
 		}
 	}
 
-	// 4. 批量查询邀请人信息
-	var usersMap = make(map[uint64]string)
-	if len(inviterUIDs) > 0 {
-		userBuilder := l.svcCtx.UserModel.SelectBuilder().Where(squirrel.Eq{"user_id": inviterUIDs})
-		users, err := l.svcCtx.UserModel.FindAll(l.ctx, userBuilder)
-		if err != nil {
-			l.Logger.Errorf("AccountBookInvitationList FindAll Users error: %v", err)
-			// 不阻断，继续
-		} else {
-			for _, user := range users {
-				// 优先使用昵称，没有则使用默认
-				usersMap[user.UserId] = user.Nickname
-			}
-		}
+	// 3. 并行查询账本信息和邀请人信息
+	var (
+		booksMap = make(map[uint64]string)
+		usersMap = make(map[uint64]string)
+	)
+
+	err = mr.Finish(func() error {
+		booksMap = l.getBooksMap(bookIDs)
+		return nil
+	}, func() error {
+		usersMap = l.getUsersMap(inviterUIDs)
+		return nil
+	})
+
+	if err != nil {
+		l.Logger.Errorf("AccountBookInvitationList mr.Finish error: %v", err)
+		// 并发执行出错不应阻断主流程，因为是辅助信息
 	}
 
-	// 5. 组装数据
+	// 4. 组装数据
+	resp = make([]types.AccountBookInvitationListResp, 0, len(invitations))
 	for _, inv := range invitations {
 		bookName := booksMap[inv.BookId]
 		if bookName == "" {
@@ -112,4 +109,42 @@ func (l *AccountBookInvitationListLogic) AccountBookInvitationList(req *types.Ac
 	}
 
 	return resp, nil
+}
+
+func (l *AccountBookInvitationListLogic) getBooksMap(bookIDs []uint64) map[uint64]string {
+	booksMap := make(map[uint64]string)
+	if len(bookIDs) == 0 {
+		return booksMap
+	}
+
+	bookBuilder := l.svcCtx.AccountBooksModel.SelectBuilder().Where(squirrel.Eq{"book_id": bookIDs})
+	books, err := l.svcCtx.AccountBooksModel.FindAll(l.ctx, bookBuilder)
+	if err != nil {
+		l.Logger.Errorf("AccountBookInvitationList FindAll Books error: %v", err)
+		return booksMap
+	}
+
+	for _, book := range books {
+		booksMap[book.BookId] = book.Name
+	}
+	return booksMap
+}
+
+func (l *AccountBookInvitationListLogic) getUsersMap(userIDs []uint64) map[uint64]string {
+	usersMap := make(map[uint64]string)
+	if len(userIDs) == 0 {
+		return usersMap
+	}
+
+	userBuilder := l.svcCtx.UserModel.SelectBuilder().Where(squirrel.Eq{"user_id": userIDs})
+	users, err := l.svcCtx.UserModel.FindAll(l.ctx, userBuilder)
+	if err != nil {
+		l.Logger.Errorf("AccountBookInvitationList FindAll Users error: %v", err)
+		return usersMap
+	}
+
+	for _, user := range users {
+		usersMap[user.UserId] = user.Nickname
+	}
+	return usersMap
 }
